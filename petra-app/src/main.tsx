9c8a7f21e6457c57394fd5ecaf92f6d2277f0d7d
Oguz Kagan EREN <oguz.eren@ceng.deu.edu.tr>
2020-01-18 11:54:18 +0300

    added graphql

--- petra-app/src/main.tsx
+++ petra-app/src/main.tsx
@@ -6,6 +6,12 @@ import { mapping, light as lightTheme } from '@eva-design/eva';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { HeaderComponent } from '../src/app/components/HeaderComponent';
+// Graphql modules
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+// Pass your GraphQL endpoint to uri
+const client = new ApolloClient({ uri: 'http://localhost:8080/v1/graphql' });
 const MyHeader = navigation => {
 	return {
 		header: props => <HeaderComponent headerTitle={navigation.getParam('title')} />
@@ -30,8 +36,11 @@ const App = createAppContainer(RootStack);
 export default () => (
 	<React.Fragment>
 		<IconRegistry icons={EvaIconsPack} />
-		<ApplicationProvider mapping={mapping} theme={lightTheme}>
-			<App />
-		</ApplicationProvider>
+		<ApolloProvider client={client}>
+			<ApplicationProvider mapping={mapping} theme={lightTheme}>
+				<App />
+			</ApplicationProvider>
+		</ApolloProvider>
+
 	</React.Fragment>
 );
