ce640b874deba7462cff763408c0202b50e7739c
Oguz Kagan EREN <oguz.eren@ceng.deu.edu.tr>
2020-01-28 19:14:08 +0300

    added AddLocationScreen and marker for map

---
+++ petra-app/src/app/pages/screens/AddLocationScreen.tsx
@@ -0,0 +1,71 @@
+import * as React from 'react';
+import { StyleSheet, Dimensions } from 'react-native';
+import { Button, Layout, Text } from '@ui-kitten/components';
+import MapView from 'react-native-maps';
+import { Marker } from 'react-native-maps';
+/**
+ * Location props
+ */
+export interface LocationProps {
+	navigation: any;
+}
+/**
+ * Location state
+ */
+export interface LocationState {
+	region: any;
+	markers: any;
+}
+
+/**
+ * Location
+ */
+export class AddLocationScreen extends React.Component<LocationProps, LocationState> {
+	constructor(props: LocationProps) {
+		super(props);
+		this.state = {
+			region: {
+				latitude: 38.4237,
+				longitude: 27.1428,
+				latitudeDelta: 0.0922,
+				longitudeDelta: 0.0421
+			},
+			markers: [
+				{
+					latlng: {
+						latitude: 38.4237,
+						longitude: 27.1428
+					},
+					title: 'test',
+					description: 'deneme'
+				}
+			]
+		};
+	}
+	onRegionChange(region) {
+		this.setState({ region });
+	}
+	/**
+	 * Renders home
+	 * @returns
+	 */
+	render() {
+		return (
+			<Layout style={{ flex: 1 }}>
+				{/* initialRegion şimdilik Izmir'i göstermektedir, kullanıcı lokasyon açarsa bulunduğu konumu göstermeli */}
+				<MapView style={styles.mapStyle} region={this.state.region} onRegionChange={this.onRegionChange.bind(this)}>
+					{this.state.markers.map(marker => (
+						<Marker coordinate={marker.latlng} title={marker.title} description={marker.description} />
+					))}
+				</MapView>
+			</Layout>
+		);
+	}
+}
+
+const styles: any = StyleSheet.create({
+	mapStyle: {
+		width: Dimensions.get('window').width,
+		height: Dimensions.get('window').height / 2
+	}
+});
