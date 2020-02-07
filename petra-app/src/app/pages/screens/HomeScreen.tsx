25566a44892bb91a8d554dce01c2d695336d70d0
Oguz Kagan EREN <oguz.eren@ceng.deu.edu.tr>
2020-02-07 09:36:08 +0300

    added scrollview

--- petra-app/src/app/pages/screens/HomeScreen.tsx
+++ petra-app/src/app/pages/screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet, Dimensions } from 'react-native';
+import { StyleSheet, Dimensions, SafeAreaView, ScrollView } from 'react-native';
 import { Button, Layout, Text } from '@ui-kitten/components';
 import { BottomComponent } from '../../components/BottomComponent';
 import { SearchComponent } from '../../components/SearchComponent';
@@ -30,46 +30,49 @@ export class HomeScreen extends React.Component<HomeProps, HomeState> {
 		const userID = this.props.navigation.getParam('userID', 'NO-ID');
 		return (
 			<Layout style={{ flex: 1 }}>
-				<SearchComponent></SearchComponent>
-				<Button
-					onPress={() => {
-						this.props.navigation.navigate('AddCompanyScreen', {
-							userID: userID
-						});
-					}}
-				>
-					Add Company
-				</Button>
-				<Button
-					onPress={() => {
-						this.props.navigation.navigate('AddRestaurantScreen', {
-							userID: userID
-						});
-					}}
-				>
-					Add Restaurant
-				</Button>
-				<Button
-					onPress={() => {
-						this.props.navigation.navigate('RestaurantDetailScreen', {
-							userID: userID
-						});
-					}}
-				>
-					Restaurant Details
-				</Button>
-				<Button
-					onPress={() => {
-						this.props.navigation.navigate('ArchSiteDetailScreen', {
-							userID: userID
-						});
-					}}
-				>
-					ArchSite Details
-				</Button>
-				<Layout style={{ position: 'absolute', left: 0, right: 0, bottom: 0 }}>
-					<BottomComponent></BottomComponent>
-				</Layout>
+				<ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+					<SearchComponent></SearchComponent>
+
+					<Button
+						onPress={() => {
+							this.props.navigation.navigate('AddCompanyScreen', {
+								userID: userID
+							});
+						}}
+					>
+						Add Company
+					</Button>
+					<Button
+						onPress={() => {
+							this.props.navigation.navigate('AddRestaurantScreen', {
+								userID: userID
+							});
+						}}
+					>
+						Add Restaurant
+					</Button>
+					<Button
+						onPress={() => {
+							this.props.navigation.navigate('RestaurantDetailScreen', {
+								userID: userID
+							});
+						}}
+					>
+						Restaurant Details
+					</Button>
+					<Button
+						onPress={() => {
+							this.props.navigation.navigate('ArchSiteDetailScreen', {
+								userID: userID
+							});
+						}}
+					>
+						ArchSite Details
+					</Button>
+					<Layout style={{ position: 'absolute', left: 0, right: 0, bottom: 0 }}>
+						<BottomComponent></BottomComponent>
+					</Layout>
+				</ScrollView>
 			</Layout>
 		);
 	}
