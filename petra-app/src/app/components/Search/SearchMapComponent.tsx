23eeb405b051482b83d59285339684a57ce8cbc7
Oguz Kagan EREN <oguz.eren@ceng.deu.edu.tr>
2020-02-23 14:07:14 +0300

    changed google map to openstreet

--- petra-app/src/app/components/Search/SearchMapComponent.tsx
+++ petra-app/src/app/components/Search/SearchMapComponent.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { StyleSheet, Dimensions } from 'react-native';
+import { StyleSheet, Dimensions, Platform } from 'react-native';
 import { BottomNavigation, Text, Icon, Layout } from '@ui-kitten/components';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView from 'react-native-maps';
 import { Marker } from 'react-native-maps';
+import { Polyline } from 'react-native-maps';
+import { UrlTile } from 'react-native-maps';
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
-import MapViewDirections from 'react-native-maps-directions';
 /**
  * Location props
  */
@@ -19,9 +20,9 @@ export interface SearchMapProps {
 export interface SearchMapState {
 	region: any;
 	markers: any;
-	mapStyle: any;
 	errorMessage: String;
 	userLoc: any;
+	polyCoords: any;
 }
 
 /**
@@ -34,269 +35,25 @@ export class SearchMapComponent extends React.Component<SearchMapProps, SearchMa
 	 */
 	constructor(props) {
 		super(props);
+
 		this.state = {
 			region: {
-				latitude: 38.4237,
-				longitude: 27.1428,
-				latitudeDelta: 0.0922,
-				longitudeDelta: 0.0421
+				latitude: 38.411258763601985,
+				longitude: 27.124096993356943,
+				latitudeDelta: 0.0028816964734517114,
+				longitudeDelta: 0.006642155349254608
 			},
 			errorMessage: '',
 			userLoc: null,
 			markers: [],
-			mapStyle: [
-				{
-					elementType: 'geometry',
-					stylers: [
-						{
-							color: '#ebe3cd'
-						}
-					]
-				},
-				{
-					elementType: 'labels.text.fill',
-					stylers: [
-						{
-							color: '#523735'
-						}
-					]
-				},
-				{
-					elementType: 'labels.text.stroke',
-					stylers: [
-						{
-							color: '#f5f1e6'
-						}
-					]
-				},
-				{
-					featureType: 'administrative',
-					elementType: 'geometry',
-					stylers: [
-						{
-							visibility: 'off'
-						}
-					]
-				},
-				{
-					featureType: 'administrative',
-					elementType: 'geometry.stroke',
-					stylers: [
-						{
-							color: '#c9b2a6'
-						}
-					]
-				},
-				{
-					featureType: 'administrative.land_parcel',
-					elementType: 'geometry.stroke',
-					stylers: [
-						{
-							color: '#dcd2be'
-						}
-					]
-				},
-				{
-					featureType: 'administrative.land_parcel',
-					elementType: 'labels.text.fill',
-					stylers: [
-						{
-							color: '#ae9e90'
-						}
-					]
-				},
-				{
-					featureType: 'landscape.natural',
-					elementType: 'geometry',
-					stylers: [
-						{
-							color: '#dfd2ae'
-						}
-					]
-				},
-				{
-					featureType: 'poi',
-					stylers: [
-						{
-							visibility: 'off'
-						}
-					]
-				},
-				{
-					featureType: 'poi',
-					elementType: 'geometry',
-					stylers: [
-						{
-							color: '#dfd2ae'
-						}
-					]
-				},
-				{
-					featureType: 'poi',
-					elementType: 'labels.text.fill',
-					stylers: [
-						{
-							color: '#93817c'
-						}
-					]
-				},
-				{
-					featureType: 'poi.park',
-					elementType: 'geometry.fill',
-					stylers: [
-						{
-							color: '#a5b076'
-						}
-					]
-				},
-				{
-					featureType: 'poi.park',
-					elementType: 'labels.text.fill',
-					stylers: [
-						{
-							color: '#447530'
-						}
-					]
-				},
-				{
-					featureType: 'road',
-					elementType: 'geometry',
-					stylers: [
-						{
-							color: '#f5f1e6'
-						}
-					]
-				},
-				{
-					featureType: 'road',
-					elementType: 'labels.icon',
-					stylers: [
-						{
-							visibility: 'off'
-						}
-					]
-				},
-				{
-					featureType: 'road.arterial',
-					elementType: 'geometry',
-					stylers: [
-						{
-							color: '#fdfcf8'
-						}
-					]
-				},
-				{
-					featureType: 'road.highway',
-					elementType: 'geometry',
-					stylers: [
-						{
-							color: '#f8c967'
-						}
-					]
-				},
-				{
-					featureType: 'road.highway',
-					elementType: 'geometry.stroke',
-					stylers: [
-						{
-							color: '#e9bc62'
-						}
-					]
-				},
-				{
-					featureType: 'road.highway.controlled_access',
-					elementType: 'geometry',
-					stylers: [
-						{
-							color: '#e98d58'
-						}
-					]
-				},
-				{
-					featureType: 'road.highway.controlled_access',
-					elementType: 'geometry.stroke',
-					stylers: [
-						{
-							color: '#db8555'
-						}
-					]
-				},
-				{
-					featureType: 'road.local',
-					elementType: 'labels.text.fill',
-					stylers: [
-						{
-							color: '#806b63'
-						}
-					]
-				},
-				{
-					featureType: 'transit',
-					stylers: [
-						{
-							visibility: 'off'
-						}
-					]
-				},
-				{
-					featureType: 'transit.line',
-					elementType: 'geometry',
-					stylers: [
-						{
-							color: '#dfd2ae'
-						}
-					]
-				},
-				{
-					featureType: 'transit.line',
-					elementType: 'labels.text.fill',
-					stylers: [
-						{
-							color: '#8f7d77'
-						}
-					]
-				},
-				{
-					featureType: 'transit.line',
-					elementType: 'labels.text.stroke',
-					stylers: [
-						{
-							color: '#ebe3cd'
-						}
-					]
-				},
-				{
-					featureType: 'transit.station',
-					elementType: 'geometry',
-					stylers: [
-						{
-							color: '#dfd2ae'
-						}
-					]
-				},
-				{
-					featureType: 'water',
-					elementType: 'geometry.fill',
-					stylers: [
-						{
-							color: '#b9d3c2'
-						}
-					]
-				},
-				{
-					featureType: 'water',
-					elementType: 'labels.text.fill',
-					stylers: [
-						{
-							color: '#92998d'
-						}
-					]
-				}
-			]
+			polyCoords: []
 		};
 	}
 	componentDidMount() {
+		const origin = { latitude: 38.4108883, longitude: 27.1232167 };
+		const destination = { latitude: 38.41493337952739, longitude: 27.130592465400692 };
 		this._getLocationAsync();
+		this.getDirections(origin, destination);
 	}
 	_getLocationAsync = async () => {
 		let { status } = await Permissions.askAsync(Permissions.LOCATION);
@@ -308,34 +65,46 @@ export class SearchMapComponent extends React.Component<SearchMapProps, SearchMa
 
 		let userLoc = await Location.getCurrentPositionAsync({});
 		this.setState({ userLoc });
-		console.log(userLoc);
+		//console.log(userLoc);
 	};
 	onRegionChange(region) {
 		console.log(region);
-		this.setState({ region });
+		//this.setState({ region });
 	}
 	_onMarkerPress(markerData) {
 		this.props.marker(markerData);
 	}
-	/*  async getDirections(startLoc, destinationLoc) {
-        try {
-             const resp = await fetch(`http://www.mapquestapi.com/directions/v2/route?key=uL4rvVLz5lqGMyPuOrDHAHV91ttOSHV9&from=38.410888,27.123216&to=38.414933,27.130592`);
-             const respJson = await resp.json();
-             if (respJson.routes.length > 0) {
-                 const points = Polyline.decode(respJson.routes[0].overview_polyline.points);
-                 const coords = points.map((point, index) => {
-                     return {
-                         latitude: point[0],
-                         longitude: point[1],
-                     };
-                 });
-                 this.setState({ coords });
-             }
-             return;
-         } catch (error) {
-             alert(error);
-         }
- } */
+	async getDirections(startPoint, endPoint) {
+		try {
+			const url =
+				'https://api.openrouteservice.org/v2/directions/driving-car?api_key=5b3ce3597851110001cf62483f947f71d8dd4b32bfb185dda3af87e2&start=' +
+				startPoint.longitude +
+				',' +
+				startPoint.latitude +
+				'&end=' +
+				endPoint.longitude +
+				',' +
+				endPoint.latitude;
+
+			const resp = await fetch(url);
+			const respJson = await resp.json();
+			//console.log('-----------');
+			//console.log(respJson.features[0].geometry.coordinates);
+			if (respJson.features[0].geometry.coordinates.length > 0) {
+				const points = respJson.features[0].geometry.coordinates;
+				const polyCoords = points.map((point, index) => {
+					return {
+						latitude: point[1],
+						longitude: point[0]
+					};
+				});
+				this.setState({ polyCoords });
+			}
+			return;
+		} catch (error) {
+			alert(error);
+		}
+	}
 	/* onMarkerChange = coordinate => {
 		const { longitude, latitude } = coordinate;
 		this.setState({ marker: coordinate });
@@ -347,17 +116,19 @@ export class SearchMapComponent extends React.Component<SearchMapProps, SearchMa
 	 * @returns
 	 */
 	render() {
-		const origin = { latitude: 38.4108883, longitude: 27.1232167 };
-		const destination = { latitude: 38.41493337952739, longitude: 27.130592465400692 };
 		return (
-			<Layout>
+			<Layout style={{ paddingBottom: -25 }}>
 				<MapView
 					style={styles.mapStyle}
-					provider={PROVIDER_GOOGLE}
-					customMapStyle={this.state.mapStyle}
+					provider={null}
+					//onRegionChange={this.onRegionChange}
 					initialRegion={this.state.region}
 					showsUserLocation={true}
 					followsUserLocation={true}
+					showsMyLocationButton={true}
+					showsCompass={true}
+					mapType={Platform.OS == 'android' ? 'none' : 'standard'}
+					userLocationAnnotationTitle="Your position"
 				>
 					{this.state.markers.map(marker => (
 						<Marker
@@ -368,6 +139,20 @@ export class SearchMapComponent extends React.Component<SearchMapProps, SearchMa
 							onPress={this._onMarkerPress.bind(this, marker)}
 						/>
 					))}
+
+					<Polyline coordinates={this.state.polyCoords} strokeWidth={5} strokeColor="#00a8ff" zIndex={16} />
+					<UrlTile
+						/**
+						 * The url template of the tile server. The patterns {x} {y} {z} will be replaced at runtime
+						 * For example, http://c.tile.openstreetmap.org/{z}/{x}/{y}.png
+						 */
+						urlTemplate={'http://c.tile.openstreetmap.org/{z}/{x}/{y}.png'}
+						/**
+						 * The maximum zoom level for this tile overlay. Corresponds to the maximumZ setting in
+						 * MKTileOverlay. iOS only.
+						 */
+						zIndex={15}
+					/>
 				</MapView>
 			</Layout>
 		);
