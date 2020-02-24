bef8265010336bb750f0eeb1949b713429024a4a
Oguz Kagan EREN <oguz.eren@ceng.deu.edu.tr>
2020-02-24 18:51:20 +0300

    changed script

--- petra-app/webpack.config.js
+++ petra-app/webpack.config.js
@@ -2,6 +2,7 @@ const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function(env, argv) {
     const config = await createExpoWebpackConfigAsync(env, argv);
+    config.resolve.alias['react-native'] = 'react-native-web';
     config.resolve.alias['react-native-maps'] = 'react-native-web-maps';
     // Customize the config before returning it.
     return config;
